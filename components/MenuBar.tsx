fc11c33f0cf7f52d922ff7ad8d4117ce870b9139
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-24 20:08:37 +0100

    update component classes

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -1,17 +1,33 @@
+'use client';
+
+import {useMemoryGameContext} from '@/context';
 import Button from './Button';
 
 const MenuBar = () => {
+    const {setShowMenu} = useMemoryGameContext();
+
     return (
-        <div className="flex justify-between">
-            <h1 className="text-(length:--fs-40) text-(--clr-blue-950) font-bold leading-(--lh-125)">
+        <div className="flex justify-between mb-(--space-500) md:mb-[8.4375rem] md:max-w-[43.0625rem] lg:max-w-[69.375rem] w-full">
+            <h1 className="text-(length:--fs-24) md:text-(length:--fs-40) text-(--clr-blue-950) font-bold leading-(--lh-125)">
                 memory
             </h1>
+
             <div className="flex gap-x-(--space-200)">
-                <Button variant="primary" name="Restart" />
+                <Button
+                    variant="primary"
+                    name="Menu"
+                    styles="text-(length:--fs-16) w-[4.875rem] py-[0.625rem] block md:hidden"
+                    onClick={() => setShowMenu(true)}
+                />
+                <Button
+                    variant="primary"
+                    name="Restart"
+                    styles="text-(length:--fs-20) w-[7.9375rem] py-[0.84375rem] hidden md:block"
+                />
                 <Button
                     variant="secondary"
                     name="New Game"
-                    styles="w-[9.3125rem]"
+                    styles="w-[9.3125rem] text-(length:--fs-20) hidden md:block"
                 />
             </div>
         </div>
