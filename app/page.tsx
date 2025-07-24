9ecd7cd76374cd7b584416aa95bbd06a1ab9d5ab
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-24 20:06:57 +0100

    update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import GameScreen from '@/components/GameScreen';
+import Landing from '@/components/Landing';
+import {useMemoryGameContext} from '@/context';
+
 export default function Home() {
-    return <div>Memory game</div>;
+    const {showGameScreen} = useMemoryGameContext();
+
+    return (
+        <main className="bg-background flex flex-col items-center justify-center">
+            {!showGameScreen ? <Landing /> : <GameScreen />}
+        </main>
+    );
 }
