a0387a4c6f2b44ee1294b1e85d0589eb800b2c92
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-25 17:57:45 +0100

    add endGame Fn to 'New Game' button

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -2,7 +2,7 @@ import {useMemoryGameContext} from '@/context';
 import Button from './Button';
 
 const MobileMenu = () => {
-    const {setShowMenu} = useMemoryGameContext();
+    const {setShowMenu, endGame} = useMemoryGameContext();
     return (
         <div className="fixed inset-0 w-full h-full bg-(--clr-black)/50 flex items-center justify-center z-10">
             <div className="flex flex-col gap-y-(--space-200) bg-(--clr-grey-100) p-(--space-300) rounded-[0.625rem] max-w-[20.4375rem] w-full">
@@ -15,6 +15,7 @@ const MobileMenu = () => {
                     variant="secondary"
                     name="New Game"
                     styles="text-(length:--fs-18) py-[0.75rem]"
+                    onClick={endGame}
                 />
                 <Button
                     variant="secondary"
