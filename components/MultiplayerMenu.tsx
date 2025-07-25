cf2737dbaa834523536e393b3c4808479fb5a50f
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-25 17:55:29 +0100

    update utility classes for responsiveness

--- components/MultiplayerMenu.tsx
+++ components/MultiplayerMenu.tsx
@@ -6,7 +6,8 @@ interface PlayerProps {
 }
 
 const MultiplayerMenu = ({player = 1, points = 0}: PlayerProps) => {
-    const showTriangleAndCurrentPlayer = player === 1 ? 'block' : 'hidden';
+    const showTriangleAndCurrentPlayer =
+        player === 1 ? 'block bg-(--clr-orange-400)' : 'block bg-transparent';
     const active =
         player === 1
             ? 'bg-(--clr-orange-400) text-(--clr-white)'
@@ -15,23 +16,24 @@ const MultiplayerMenu = ({player = 1, points = 0}: PlayerProps) => {
         player === 1 ? 'text-(--clr-white)' : 'text-(--clr-blue-800)';
 
     return (
-        <div className="flex flex-col gap-y-(--space-200) w-[15.9375rem] font-bold leading-(--lh-125)">
+        <div className="flex flex-col gap-y-(--space-200) w-[4rem] md:w-[10.375rem] xl:w-[15.9375rem] font-bold leading-(--lh-125)">
             <div className="flex flex-col items-center justify-center">
                 <div
-                    className={`triangle ${showTriangleAndCurrentPlayer}`}></div>
+                    className={`triangle w-(--space-200) md:w-(--space-300) lg:w-[2.4099rem] h-(--space-100) md:h-[0.75rem] lg:h-[1.24049rem] ${showTriangleAndCurrentPlayer}`}></div>
                 <div
-                    className={` ${active} flex items-center justify-between  px-[1.4063rem] py-(--space-100) h-[4.5rem] rounded-[0.625rem] w-full`}>
-                    <span className="text-(length:--fs-18)">
-                        Player {player}
+                    className={` ${active} flex flex-col xl:flex-row items-center justify-between md:items-start xl:items-center md:gap-y-[1.125rem] md:justify-normal xl:justify-between py-[0.6563rem] md:py-[0.75rem] lg:py-(--space-100) md:px-(--space-200) h-[4.5rem] md:h-[5rem] rounded-[0.625rem] w-full`}>
+                    <span className=" text-(length:--fs-15) md:text-(length:--fs-18)">
+                        P<span className="hidden md:inline">layer </span>
+                        {player}
                     </span>
                     <span
-                        className={`text-(length:--fs-32) ${pointsTextColor} `}>
+                        className={`text-(length:--fs-24) lg:text-(length:--fs-32) ${pointsTextColor} `}>
                         {points}
                     </span>
                 </div>
             </div>
             <p
-                className={` ${showTriangleAndCurrentPlayer} text-(length:--fs-13) text-(--clr-blue-950) text-center uppercase tracking-(--ls-5)`}>
+                className={` hidden xl:block text-(length:--fs-13) text-(--clr-blue-950) text-center uppercase tracking-(--ls-5)`}>
                 Current turn
             </p>
         </div>
