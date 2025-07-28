cb50e1c33caf312e5a2970fcd486588357c953b6
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-28 17:09:41 +0100

    import formatTime Fn from utils.ts

--- components/SoloMenu.tsx
+++ components/SoloMenu.tsx
@@ -1,10 +1,6 @@
 'use client';
 
-const formatTime = (seconds: number) => {
-    const minutes = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${minutes}:${secs.toString().padStart(2, '0')}`;
-};
+import {formatTime} from '@/utils';
 
 const SoloMenu = ({time, moves}: {time: number; moves: number}) => {
     return (
