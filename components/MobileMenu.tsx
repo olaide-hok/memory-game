f1b39780180d16f6b57772957fc8d58315bb541e
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-28 10:49:37 +0100

    add onClick handler to "Restart" button

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -2,7 +2,7 @@ import {useMemoryGameContext} from '@/context';
 import Button from './Button';
 
 const MobileMenu = () => {
-    const {setShowMenu, endGame} = useMemoryGameContext();
+    const {setShowMenu, endGame, restartGame} = useMemoryGameContext();
     return (
         <div className="fixed inset-0 w-full h-full bg-(--clr-black)/50 flex items-center justify-center z-10">
             <div className="flex flex-col gap-y-(--space-200) bg-(--clr-grey-100) p-(--space-300) rounded-[0.625rem] max-w-[20.4375rem] w-full">
@@ -10,6 +10,7 @@ const MobileMenu = () => {
                     variant="primary"
                     name="Restart"
                     styles="text-(length:--fs-18) py-[0.75rem]"
+                    onClick={restartGame}
                 />
                 <Button
                     variant="secondary"
