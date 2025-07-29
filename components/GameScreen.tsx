c246dcb87a14f4cc00bc8fa4d340afe0408133ca
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-29 18:43:05 +0100

    feat: render ResultCard for multiplayers.
    update MultiplayerMenu renderingto use pairsMatched state value.

--- components/GameScreen.tsx
+++ components/GameScreen.tsx
@@ -7,10 +7,21 @@ import GameGrid from './GameGrid';
 import MultiplayerMenu from './MultiplayerMenu';
 import SoloMenu from './SoloMenu';
 import SoloGameResult from './SoloGameResult';
+import ResultCard from './ResultCard';
 
 const GameScreen = () => {
-    const {game, time, moves, showMenu, gameOverModal, restartGame, endGame} =
-        useMemoryGameContext();
+    const {
+        game,
+        time,
+        moves,
+        showMenu,
+        gameOverModal,
+        restartGame,
+        endGame,
+        currentPlayer,
+        pairsMatched,
+        isMultiplayer,
+    } = useMemoryGameContext();
 
     return (
         <div className="container flex flex-col items-center py-(--space-300) md:py-(--space-600) px-(--space-300) md:px-[2.5rem] md:pt-[2.3125rem] lg:pt-[4.1875rem] w-full">
@@ -22,13 +33,16 @@ const GameScreen = () => {
             {/* Player tiles */}
             {game.numOfPlayers > 1 ? (
                 <div className="flex items-center  gap-x-(--space-300)">
-                    {Array.from({length: game.numOfPlayers}).map((_, index) => (
-                        <MultiplayerMenu
-                            key={index}
-                            player={index + 1}
-                            points={0}
-                        />
-                    ))}
+                    {Object.entries(pairsMatched)
+                        .filter(([id]) => parseInt(id) <= game.numOfPlayers)
+                        .map(([id, move]) => (
+                            <MultiplayerMenu
+                                key={id}
+                                player={id}
+                                points={move}
+                                currentPlayer={currentPlayer}
+                            />
+                        ))}
                 </div>
             ) : (
                 <SoloMenu time={time} moves={moves} />
@@ -38,14 +52,23 @@ const GameScreen = () => {
             {showMenu && <MobileMenu />}
 
             {/* Game Over Modal */}
-            {gameOverModal && (
-                <SoloGameResult
-                    time={time}
-                    moves={moves}
-                    restartGame={restartGame}
-                    endGame={endGame}
-                />
-            )}
+            {gameOverModal &&
+                (isMultiplayer ? (
+                    <ResultCard
+                        type="multiplayer"
+                        pairsMatched={pairsMatched}
+                        restartGame={restartGame}
+                        endGame={endGame}
+                        numOfPlayers={game.numOfPlayers}
+                    />
+                ) : (
+                    <SoloGameResult
+                        time={time}
+                        moves={moves}
+                        restartGame={restartGame}
+                        endGame={endGame}
+                    />
+                ))}
         </div>
     );
 };
