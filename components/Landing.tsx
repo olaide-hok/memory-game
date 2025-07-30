db5de7bd4b333dea4a6478c0f140a191bcb6f8df
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-07-30 13:34:27 +0100

    update utility classes

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -2,7 +2,7 @@ import StartGameCard from './StartGameCard';
 
 const Landing = () => {
     return (
-        <div className="contianer flex flex-col items-center justify-center bg-(--clr-blue-950) gap-y-(--space-600) md:gap-y-(--space-1000) h-screen w-full">
+        <div className="contianer flex flex-col items-center bg-(--clr-blue-950) gap-y-(--space-600) md:gap-y-(--space-1000) h-screen w-full pt-[6rem] md:pt-[10.4375rem]">
             <h1 className="text-(length:--fs-32) md:text-(length:--fs-40) text-(--clr-grey-50) font-bold leading-(--lh-125)">
                 memory
             </h1>
